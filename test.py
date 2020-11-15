6ac64ae5dc833fceadbac3df2fbed5968ad047c0
Jeremy A Gray <jeremy.a.gray@gmail.com>
2020-11-15 12:24:37 -0600

    chore: fix linting errors
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
+# import matplotlib.pyplot as plt
+# import numpy as np
+# import seaborn as sns
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import numpy as np
 
 # Import data.
 df = pd.read_csv('medical_examination.csv')
@@ -16,10 +16,12 @@ df['overweight'] = df.apply(lambda row: 1 if row.bmi > 25 else 0, axis=1)
 # value of 'cholestorol' or 'gluc' is 1, make the value 0. If the
 # value is more than 1, make the value 1.
 df['gluc'] = df.apply(lambda row: 0 if row.gluc == 1 else 1, axis=1)
-df['cholesterol'] = df.apply(lambda row: 0 if row.cholesterol == 1 else 1, axis=1)
+df['cholesterol'] = df\
+    .apply(lambda row: 0 if row.cholesterol == 1 else 1, axis=1)
 
-# Narrow the data set. 
-# df1 = df.loc[:, ['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight', 'cardio']]
+# Narrow the data set.
+# df1 = df.loc[:, ['cholesterol', 'gluc', 'smoke', 'alco',
+#                  'active', 'overweight', 'cardio']]
 # df1 = df1.melt(id_vars=['cardio'])
 
 # # Regroup by 'cardio' and sum.
@@ -28,7 +30,8 @@ df['cholesterol'] = df.apply(lambda row: 0 if row.cholesterol == 1 else 1, axis=
 # df1 = df1.reset_index(name='totals')
 
 # # Draw the catplot with sns.catplot().
-# fig = sns.catplot(data=df1, kind="bar", x="variable", y="totals", hue="value", col='cardio')
+# fig = sns.catplot(data=df1, kind="bar", x="variable", y="totals",
+#                   hue="value", col='cardio')
 
 # # Do not modify the next two lines
 # fig.savefig('catplot.png')
@@ -38,23 +41,22 @@ df['cholesterol'] = df.apply(lambda row: 0 if row.cholesterol == 1 else 1, axis=
 # def draw_heat_map():
 # Clean the data.
 df1 = df.loc[:,
-                [
-                    'id',
-                    'age',
-                    'gender',
-                    'height',
-                    'weight',
-                    'ap_hi',
-                    'ap_lo',
-                    'cholesterol',
-                    'gluc',
-                    'smoke',
-                    'alco',
-                    'active',
-                    'cardio',
-                    'overweight',
-                ]
-]
+             [
+                 'id',
+                 'age',
+                 'gender',
+                 'height',
+                 'weight',
+                 'ap_hi',
+                 'ap_lo',
+                 'cholesterol',
+                 'gluc',
+                 'smoke',
+                 'alco',
+                 'active',
+                 'cardio',
+                 'overweight',
+             ]]
 
 df1 = df1.loc[df['ap_lo'] <= df['ap_hi']]
 df1 = df1.loc[df['height'] >= df['height'].quantile(0.025)]
@@ -63,23 +65,22 @@ df1 = df1.loc[df['weight'] >= df['weight'].quantile(0.025)]
 df1 = df1.loc[df['weight'] <= df['weight'].quantile(0.975)]
 
 df2 = df.loc[:,
-                [
-                    'id',
-                    'age',
-                    'gender',
-                    'height',
-                    'weight',
-                    'ap_hi',
-                    'ap_lo',
-                    'cholesterol',
-                    'gluc',
-                    'smoke',
-                    'alco',
-                    'active',
-                    'cardio',
-                    'overweight',
-                ]
-]
+             [
+                 'id',
+                 'age',
+                 'gender',
+                 'height',
+                 'weight',
+                 'ap_hi',
+                 'ap_lo',
+                 'cholesterol',
+                 'gluc',
+                 'smoke',
+                 'alco',
+                 'active',
+                 'cardio',
+                 'overweight',
+             ]]
 
 df2 = df2.loc[
     (df['ap_lo'] <= df['ap_hi'])
