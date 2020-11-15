0dfd60bb8f2c915ee8c91d69d45d98972d68e6b7
Jeremy A Gray <jeremy.a.gray@gmail.com>
2020-11-15 12:23:59 -0600

    chore: add pytest and linting
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((python-mode
+  (flycheck-python-flake8-executable . "/home/gray/.virtualenvs/fcc-da-medical-data-visualizer/bin/flake8")
+  (flycheck-python-pylint-executable . "/home/gray/.virtualenvs/fcc-da-medical-data-visualizer/bin/flake8")))
